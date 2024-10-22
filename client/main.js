329537989e8473f3c4661525673c163a84c5411b
BlueInfinity1 <koaxxe@gmail.com>
2024-10-22 14:13:50 +0300

    Changed "Meow Wars" in resource URLs

--- client/main.js
+++ client/main.js
@@ -42,11 +42,11 @@ function initializeUnityInstance(authData) {
         console.log("MAIN.JS: Using standard URLs outside Discord environment.");
     }
 
-    const loaderUrl = `${buildUrl}/Oct 20th.loader.js`;
+    const loaderUrl = `${buildUrl}/Meow Wars.loader.js`;
     const config = {
-        dataUrl: `${buildUrl}/Oct 20th.data`,
-        frameworkUrl: `${buildUrl}/Oct 20th.framework.js`,
-        codeUrl: `${buildUrl}/Oct 20th.wasm`,
+        dataUrl: `${buildUrl}/Meow Wars.data`,
+        frameworkUrl: `${buildUrl}/Meow Wars.framework.js`,
+        codeUrl: `${buildUrl}/Meow Wars.wasm`,
         streamingAssetsUrl,
         companyName: "SuperSocialLabs",
         productName: "Meow Wars",
@@ -63,7 +63,7 @@ function initializeUnityInstance(authData) {
     script.src = loaderUrl;
     script.onload = () => {
         createUnityInstance(canvas, config, progress => {
-            console.log("MAIN.JS: Unity loading progress:", progress * 100 + "%");
+            //console.log("Unity loading progress:", progress * 100 + "%");
             progressBarFull.style.width = 100 * progress + "%";
         }).then(instance => {
             unityInstance = instance;
@@ -99,7 +99,8 @@ async function setupDiscordSdk(discordSdk) {
 
     console.log("MAIN.JS: Received Discord authorization code:", code);
 
-    const response = await fetch("/.proxy/api/token", {
+    // Exchange code for access token via your proxy endpoint
+    const response = await fetch("/.proxy/aws/Authenticate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ code }),
@@ -108,26 +109,57 @@ async function setupDiscordSdk(discordSdk) {
     const { access_token } = await response.json();
     console.log("MAIN.JS: Received access token:", access_token);
 
-    auth = await discordSdk.commands.authenticate({ access_token });
-    if (!auth) {
-        throw new Error("MAIN.JS: Authentication with Discord failed.");
-    }
-
-    console.log("MAIN.JS: Authenticated with Discord:", auth);
-
-    const member = await fetch(`https://discord.com/api/v10/users/@me/guilds/${discordSdk.guildId}/member`, {
+    // Fetch the user's global information (for global user ID and name)
+    const userInfo = await fetch('https://discord.com/api/v10/users/@me', {
         headers: {
-            Authorization: `Bearer ${auth.access_token}`,
+            Authorization: `Bearer ${access_token}`,
             "Content-Type": "application/json",
         },
     }).then(response => response.json());
 
-    const username = member?.nick ?? auth.user.global_name;
-    const iconUrl = member?.avatar
-        ? `https://cdn.discordapp.com/guilds/${discordSdk.guildId}/users/${auth.user.id}/avatars/${member.avatar}.png?size=256`
-        : `https://cdn.discordapp.com/avatars/${auth.user.id}/${auth.user.avatar}.png?size=256`;
+    // Fetch the user's guilds to get the guild ID and name
+    const guilds = await fetch('https://discord.com/api/v10/users/@me/guilds', {
+        headers: {
+            Authorization: `Bearer ${access_token}`,
+            "Content-Type": "application/json",
+        },
+    }).then(response => response.json());
 
-    console.log("MAIN.JS: User authenticated as:", username, "with avatar URL:", iconUrl);
+    // Check if we are in a guild or a private/group chat
+    let guildId, guildName, member = null;
+    const currentGuild = guilds.find(g => g.id === discordSdk.guildId);
 
-    return { username, iconUrl, accessToken: access_token };
+    if (currentGuild) {
+        // In a guild
+        guildId = currentGuild.id;
+        guildName = currentGuild.name;
+
+        // Fetch the user's member data in the guild (for username in guild and avatar)
+        member = await fetch(`https://discord.com/api/v10/users/@me/guilds/${guildId}/member`, {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+                "Content-Type": "application/json",
+            },
+        }).then(response => response.json());
+
+    } else {
+        // In a private chat or group chat, use "u" + userId as the guildId and username as the guildName
+        guildId = "u" + userInfo.id;
+        guildName = userInfo.username;
+    }
+
+    // Correctly extract the username based on guild-specific nickname and global name
+    const username = member?.nick ?? userInfo.global_name ?? userInfo.username;
+
+    // Extract required data: user ID, username, guild ID, and nickname
+    const userId = userInfo.id;
+
+    // Print out the details
+    console.log("User ID:", userId);
+    console.log("Global Username:", userInfo.username);
+    console.log("Guild ID:", guildId);
+    console.log("Guild Name:", guildName);
+    console.log("Username in Guild (Nickname):", username);
+
+    return { userId, username, guildId, guildName };
 }
